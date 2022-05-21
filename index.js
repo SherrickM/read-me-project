25c46a28056a5408c8589b98f2ee5877811e18b1
Marquis <smcneal89@gmail.com>
2022-05-21 10:29:26 -0400

    added functions &  questions

--- index.js
+++ index.js
@@ -1,19 +1,67 @@
 // TODO: Include packages needed for this application
-const fakeStuff = require('./utils/fakeStuff.js');
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input', 
+    message: "What is your GitHub username? ", 
+    name: 'username'
+  },
+  {
+    type: 'input', 
+    message: "What is your e-mail address? ", 
+    name: 'email'
+  },
+  {
+    type: 'input', 
+    message: "What is the title of your project? ", 
+    name: 'title'
+  },
+  {
+    type: 'input', 
+    message: "Please write a short description of your project: ", 
+    name: 'description'
+  },
+  {
+    type: 'list', 
+    message: "What kind of license should your project have?", 
+    name: "license",
+    choices:['Apache_2.0','MIT','Eclipse_Public_License_1.0','None']
+  },
+  {
+    type: 'input', 
+    message: "What command should be run to run test? (NPM)", 
+    name: 'test'
+  },
+  {
+    type: 'input', 
+    message: "What does the user need to know about using the repo?", 
+    name: 'repoInstuctions'
+  },
+  {
+    type: 'input', 
+    message: "What does the user need to know about contributing to the repo?", 
+    name: 'repoContributing'
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(),fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((responses)=> {
+    console.log(responses)
+    writeToFile('README.md', generateMarkdown(responses))
+  })
+}
 
 // Function call to initialize app
 init();
 
-var data = {
-  numFakes: 2
-}
-console.log(fakeStuff(data));
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,23 +1,91 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "None") {
+    return "";
+  }
+  return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "None") {
+    return "";
+  }
+  return `* [License](#license)`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return "";
+  }
+  return `## License
+  
+  This project is licensed under ${license} license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   // data should have a license badge, license link, license section
   // render functions are going to be used here
   // you have to figure out why...
-  return `# ${data.title}
-
-`;
+  return `# ${data.title} 
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description
+  
+ ${data.description}
+  
+  ## Table of Contents 
+  
+  * [Installation](#installation)
+  
+  * [Usage](#usage)
+  
+  ${renderLicenseLink(data.license)}
+  
+  * [Contributing](#contributing)
+  
+  * [Tests](#tests)
+  
+  * [Questions](#questions)
+  
+  ## Installation
+  
+  To install necessary dependencies, run the following command:
+  
+  \`\`\`
+ ${data.test}
+  \`\`\`
+  
+  ## Usage
+  
+  ${data.repoInstuctions}
+  
+ ${renderLicenseSection(data.license)}
+    
+  ## Contributing
+  
+ ${data.repoContributing}
+  
+  ## Tests
+  
+  To run tests, run the following command:
+  
+\`\`\`
+  ${data.test}
+\`\`\`
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact me directly at ${
+    data.email
+  } You can find more of my work at [${data.username}](https://github.com/${
+    data.username
+  }/).`;
 }
 
 module.exports = generateMarkdown;
